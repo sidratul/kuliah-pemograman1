8c7d2324c357dca9d161285c1f378e0cfcddd50d
muhammad sidratul muntaha 4510210013 <tear_ofregret@hotmail.co.id>
2011-10-05 12:07:41 +0700

    sesi 3

---
+++ sesi 3/soal_3/belajar.java
@@ -0,0 +1,9 @@
+public class belajar{
+	public static void main(String[] args){
+		int ofnumber =10;
+		double ofdouble = 20;
+		
+		System.out.println(ofnumber);
+		System.out.println(ofdouble);
+	}
+}
