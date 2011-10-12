965308cd597d205c1676079476c94f1c7e6f3389
muhammad sidratul muntaha 4510210013 <tear_ofregret@hotmail.co.id>
2011-10-12 15:06:16 +0700

    sesi 5

---
+++ sesi 5/soal1/laptopmain.java
@@ -0,0 +1,48 @@
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class laptopmain{
+	public static void main(String[] agrs){
+		File file=new File("D:/muhammad sidratul muntaha/pemograman 1/sesi 5/soal1/laptop.txt");
+		FileInputStream fis = null;
+		BufferedInputStream bis=null;
+		DataInputStream dis =null;
+		List<laptop> listlaptop = new ArrayList<laptop>();
+		
+		try{
+			fis = new FileInputStream(file);
+			bis = new BufferedInputStream(fis);
+			dis = new DataInputStream(bis);
+			
+			while(dis.available()!=0){
+				//memecah text perbaris menjadi beberapa kolom sesuai dengan pemisah koma
+				String line = dis.readLine();
+				String[] data = line.split(",");
+				
+				laptop l = new laptop();
+				l.setMerk(data[0]);
+				l.setProcessor(data [1]);
+				l.setHarga(data[2]);
+				
+				listlaptop.add(l);
+			}
+		}
+		
+		catch(IOException ex){
+			System.out.println("File" + file.getName()+"tidak ketemu !");
+			System.out.println("Exception : "+ex.getMessage());
+		}
+		
+		for(laptop lp : listlaptop){
+			System.out.println("========================");
+			System.out.println("merk : "+lp.getMerk());
+			System.out.println("Processor : "+lp.getProcessor());
+			System.out.println("Harag : "+lp.getHarga());
+		}
+	}	
+}
