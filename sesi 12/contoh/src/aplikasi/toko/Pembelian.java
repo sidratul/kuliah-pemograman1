95d9a6e705c3a989aee0c53b3ca9567f88737dfb
muhammad sidratul muntaha 4510210013 <tear_ofregret@hotmail.co.id>
2011-12-07 14:37:21 +0700

    ulm dan contohnya

---
+++ sesi 12/contoh/src/aplikasi/toko/Pembelian.java
@@ -0,0 +1,10 @@
+package aplikasi.toko;
+
+import java.util.Date;
+import java.util.List;
+
+public class Pembelian{
+	private Date tglTransaksi;
+	private List<PembelianDetail> daftarPemeblianDetail;
+	
+}
---
+++ sesi 12/contoh/src/aplikasi/toko/PembelianDetail.java
@@ -0,0 +1,7 @@
+package aplikasi.toko;
+
+public class PembelianDetail{
+	private Integer jumlah;
+	private Pembelian p;
+	private Product produk;
+}
